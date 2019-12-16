6d6070e49ad01e28d5b3286aae91b3d203dd437f
david <da.moura@campus.fct.unl.pt>
2019-12-16 12:23:42 +0000

    added get single player

--- server/routes/player.js
+++ server/routes/player.js
@@ -2,17 +2,23 @@ const express = require('express');
 const router = new express.Router();
 const connection = require('../db/mysql.js');
 router.get('/players', (req, res) => {
-    connection.query('SELECT * FROM Player', function (err, result) {
-        if (err){
+    let q = 'SELECT * FROM Player';
+    connection.query(q, function (err, result) {
+        if (err) {
             res.status(400).send('DB error')
         }
         res.send((result));
-
     })
 })
 
 router.get('/players/:playerId', (req, res) => {
-    res.send(/* send specific player */);
+    let q = 'SELECT * FROM Player WHERE Player.idPlayer = ?';
+    connection.query(q, [req.params.playerId], function (err, result) {
+        if (err) {
+            res.status(400).send('DB error')
+        }
+        res.send((result));
+    })
 })
 
 router.post('/players', (req, res) => {
