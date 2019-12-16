1bd0df2cf98dd40e1576f57bd6c2b31d672b62f9
david <da.moura@campus.fct.unl.pt>
2019-12-16 11:23:09 +0000

    added player router to server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const peripheralRouter = require('./routes/peripheral');
+const playerRouter = require('./routes/player');
 const app = express()
 
 app.use(express.json());
 app.use(express.static('./public')); //Send index.html page on GET /
 
 app.use(peripheralRouter)
-
+app.use(playerRouter)
 app.listen(3000)
