15efce5a0e1c8cd3b6bab68f7755005e72ad4021
david <da.moura@campus.fct.unl.pt>
2019-12-16 12:34:09 +0000

    added query to get all teams

--- server/routes/team.js
+++ server/routes/team.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = new express.Router();
 
+// TODO refactor select *
 router.get('/teams', (req, res) => {
-    res.send(/* send all teams */);
+    let q = 'SELECT * FROM Team';
+    connection.query(q, function (err, result) {
+        if (err) {
+            res.status(400).send('DB error')
+        }
+        res.send((result));
+    })
 })
 
 router.get('/teams/:teamId', (req, res) => {
