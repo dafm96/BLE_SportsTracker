e05b3d52b9d10270ab4fdc5177d3c01a0357c7ab
david <da.moura@campus.fct.unl.pt>
2019-12-16 12:33:30 +0000

    Added team router to server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const peripheralRouter = require('./routes/peripheral');
 const playerRouter = require('./routes/player');
+const teamRouter = require('./routes/team');
 const app = express()
 
 app.use(express.json());
 app.use(express.static('./public')); //Send index.html page on GET /
 
-app.use(peripheralRouter)
-app.use(playerRouter)
+app.use(peripheralRouter);
+app.use(playerRouter);
+app.use(teamRouter);
 app.listen(3000)
