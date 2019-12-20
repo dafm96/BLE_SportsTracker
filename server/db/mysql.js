47886ee5314247590c8dadbfe8b275fa85bded31
david <da.moura@campus.fct.unl.pt>
2019-12-20 11:02:17 +0000

    added nodemon to dev

--- server/db/mysql.js
+++ server/db/mysql.js
@@ -11,8 +11,6 @@ connection.connect(function (err) {
         console.error('error connecting: ' + err.stack);
         return;
     }
-
-    console.log('connected as id ' + connection.threadId);
 });
 
 module.exports = connection
