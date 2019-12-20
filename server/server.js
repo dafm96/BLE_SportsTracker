b30b1310e612442d21640a293a076962217ef383
david <da.moura@campus.fct.unl.pt>
2019-12-20 17:07:54 +0000

    added game router to server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const peripheralRouter = require('./routes/peripheral');
 const playerRouter = require('./routes/player');
 const teamRouter = require('./routes/team');
+const gameRouter = require('./routes/game');
 const app = express()
 const broker = require('./broker')
 
@@ -11,4 +12,5 @@ app.use(express.static('./public')); //Send index.html page on GET /
 app.use(peripheralRouter);
 app.use(playerRouter);
 app.use(teamRouter);
+app.use(gameRouter);
 app.listen(3000)
