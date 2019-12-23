ef47eb850389d1ea41a74537d1ee3ae02c4a8328
david <da.moura@campus.fct.unl.pt>
2019-12-23 15:26:32 +0000

    Implemented TeamDetails

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import PlotComponent from "./PlotComponent";
+import TeamList from "./Teams/TeamList";
 import TeamDetailComponent from './Teams/TeamDetail';
 import {
   BrowserRouter as Router,
---
+++ client/src/Teams/TeamDetail/index.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
+import { Row, Col, ListGroup, Jumbotron, Container } from 'react-bootstrap';
+const API = '/teams/';
+
+class TeamDetailComponent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            teamId: props.match.params.teamId,
+            team: {},
+            isLoading: true,
+            error: null,
+        };
+    }
+
+    componentDidMount() {
+        this.setState({ isLoading: true });
+        fetch(API + this.state.teamId + '/players')
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Something went wrong ...');
+                }
+            })
+            .then(data => this.setState({ team: data, isLoading: false }))
+            .catch(error => this.setState({ error, isLoading: false }));
+    }
+
+    render() {
+        const { team, isLoading, error } = this.state;
+
+        return (
+            <Container>
+                {error && <p>{error.message}</p>
+                }
+                {isLoading ?
+                    <div className="text-center">
+                        <Spinner animation="border" role="status">
+                            <span className="sr-only">Loading...</span>
+                        </Spinner>
+                    </div>
+                    :
+                    <>
+                        <Jumbotron fluid className="text-center">
+                            <h1>{team.teamName}</h1>
+                        </Jumbotron>
+                        <h3 >Players:</h3>
+                        <ListGroup >
+                            {team.players.map(player =>
+                                <ListGroup.Item key={player.idPlayer}>
+                                    
+                                    {player.playerName}
+                                </ListGroup.Item>
+                            )}
+                        </ListGroup>
+                    </>
+                }
+            </Container>
+        );
+
+    }
+}
+
+export default TeamDetailComponent
