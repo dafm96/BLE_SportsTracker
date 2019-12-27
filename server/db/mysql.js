e8dc128206f1d86495c29433928eaf4b72d493ef
David <dafm1996@gmail.com>
2019-12-27 18:59:00 +0000

    changed db password

--- server/db/mysql.js
+++ server/db/mysql.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'password',
+    password: 'root',
     database: 'BLE_Sports_Tracker'
 });
 
