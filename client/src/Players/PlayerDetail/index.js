5982892429aff151e6836857e9d112266ad73aea
David <dafm1996@gmail.com>
2019-12-30 23:27:04 +0000

    added player detail component

---
+++ client/src/Players/PlayerDetail/index.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
+import { Row, Col, ListGroup, Jumbotron, Container } from 'react-bootstrap';
+const API = '/players/';
+
+class PlayerDetailComponent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            playerId: props.match.params.playerId,
+            player: {},
+            isLoading: true,
+            error: null,
+        };
+    }
+
+    componentDidMount() {
+        this.setState({ isLoading: true });
+        fetch(API + this.state.playerId)
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Something went wrong ...');
+                }
+            })
+            .then(data => this.setState({ player: data[0], isLoading: false }))
+            .catch(error => this.setState({ error, isLoading: false }));
+    }
+
+    render() {
+        const { player, isLoading, error } = this.state;
+
+        return (
+            <Container>
+                {error && <p>{error.message}</p>
+                }
+                {isLoading ?
+                    <div className="text-center">
+                        <Spinner animation="border" role="status">
+                            <span className="sr-only">Loading...</span>
+                        </Spinner>
+                    </div>
+                    :
+                    <>
+                        <Jumbotron fluid className="text-center">
+                            <h1>{player.playerName}</h1>
+                        </Jumbotron>
+                        <ListGroup >
+                            <h2>Team: {player.teamName}</h2>
+                        </ListGroup>
+                    </>
+                }
+            </Container>
+        );
+
+    }
+}
+
+export default PlayerDetailComponent
