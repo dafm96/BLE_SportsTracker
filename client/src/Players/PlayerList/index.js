0ba7f879b1021b7de8a2485367eb561724469cff
David <dafm1996@gmail.com>
2020-01-03 16:21:12 +0000

    moved add player modal from this file

--- client/src/Players/PlayerList/index.js
+++ client/src/Players/PlayerList/index.js
@@ -1,110 +1,8 @@
 import React, { Component } from 'react';
 import Spinner from 'react-bootstrap/Spinner';
-import { Form, ListGroup, Container, Jumbotron, Button, ButtonToolbar, Modal } from 'react-bootstrap';
+import { ListGroup, Container, Jumbotron, Button, ButtonToolbar } from 'react-bootstrap';
+import AddPlayerModal from './AddPlayer';
 const API = '/players';
-
-class NameForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { playerName: '', playerTeamId: -1, teams: [] };
-        this.handleNameChange = this.handleNameChange.bind(this);
-        this.handleTeamIdChange = this.handleTeamIdChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    handleNameChange(event) {
-        this.setState({ playerName: event.target.value });
-    }
-
-    handleTeamIdChange(event) {
-        this.setState({ playerTeamId: event.target.value });
-    };
-
-    handleSubmit = event => {
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
-        event.preventDefault();
-        fetch('/players', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                "playerName": this.state.playerName,
-                "teamId": this.state.playerTeamId
-            })
-        })
-            .then(response => {
-                if (response.ok) {
-                    return window.open("/players", "_self");
-                    // return response.json();
-                } else {
-                    throw new Error('Something went wrong ...');
-                }
-            })
-    }
-
-    componentDidMount() {
-        fetch('/teams')
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error('Something went wrong ...');
-                }
-            })
-            .then(data => this.setState({ teams: data, playerTeamId: data[0].idTeam}))
-            .catch(error => console.log(error, 'error'));
-    }
-
-
-    render() {
-        return (
-            <Form onSubmit={this.handleSubmit}>
-                <Form.Group>
-                    <Form.Label>Player name</Form.Label>
-                    <Form.Control
-                        required
-                        type="text"
-                        placeholder="Player Name"
-                        onChange={this.handleNameChange}
-                    />
-                </Form.Group>
-                <Form.Group controlId="exampleForm.ControlSelect1">
-                    <Form.Label>Select Team</Form.Label>
-                    <Form.Control
-                        required
-                        as="select"
-                        onChange={this.handleTeamIdChange}>
-                        {console.log(this.state.playerTeamId)}
-                        {this.state.teams.map(team => <option key={team.idTeam} value={team.idTeam}>{team.teamName}</option>)}
-                    </Form.Control>
-                </Form.Group>
-                <Button type="submit">Submit form</Button>
-            </Form>
-        );
-    }
-}
-
-function MyVerticallyCenteredModal(props) {
-    return (
-        <Modal
-            {...props}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-        >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    New Player
-          </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <NameForm />
-            </Modal.Body>
-        </Modal>
-    );
-}
 
 class PlayerListComponent extends Component {
 
@@ -156,7 +54,7 @@ class PlayerListComponent extends Component {
                                 Add New Player
                             </Button>
 
-                            <MyVerticallyCenteredModal
+                            <AddPlayerModal
                                 show={this.state.modalShow}
                                 onHide={() => this.setState({ modalShow: false })}
                             />
