c6ed781ed5e95da49edaafdb443bc6e0f19a7e1c
David <dafm1996@gmail.com>
2020-01-03 16:20:57 +0000

    moved add team modal from this file

--- client/src/Teams/TeamList/index.js
+++ client/src/Teams/TeamList/index.js
@@ -1,83 +1,8 @@
 import React, { Component } from 'react';
 import Spinner from 'react-bootstrap/Spinner';
-import { Form, ListGroup, Container, Jumbotron, Button, ButtonToolbar, Modal } from 'react-bootstrap';
+import { ListGroup, Container, Jumbotron, Button, ButtonToolbar } from 'react-bootstrap';
+import AddTeamModal from './AddTeam';
 const API = '/teams';
-
-class NameForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { teamName: '' };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    handleChange(event) {
-        this.setState({ teamName: event.target.value });
-    }
-
-    handleSubmit = event => {
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
-        console.log(this.state.teamName)
-        event.preventDefault();
-        fetch('/teams', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                "teamName": this.state.teamName
-            })
-        })
-            .then(response => {
-                if (response.ok) {
-                    return window.open("/teams", "_self");
-                    // return response.json();
-                } else {
-                    throw new Error('Something went wrong ...');
-                }
-            })
-
-
-    }
-
-    render() {
-        return (
-            <Form onSubmit={this.handleSubmit}>
-            <Form.Group>
-                <Form.Label>Team name</Form.Label>
-                <Form.Control
-                    required
-                    type="text"
-                    placeholder="Team name"
-                    onChange={this.handleChange}
-                />
-                </Form.Group>
-                <Button type="submit">Submit form</Button>
-            </Form>
-        );
-    }
-}
-
-function MyVerticallyCenteredModal(props) {
-    return (
-        <Modal
-            {...props}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-        >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    New Team
-          </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <NameForm />
-            </Modal.Body>
-        </Modal>
-    );
-}
 
 class TeamListComponent extends Component {
 
@@ -128,7 +53,7 @@ class TeamListComponent extends Component {
                                 Add New Team
                             </Button>
 
-                            <MyVerticallyCenteredModal
+                            <AddTeamModal
                                 show={this.state.modalShow}
                                 onHide={() => this.setState({ modalShow: false })}
                             />
