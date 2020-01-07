736c8b9e1f1de4fe611b8e05015ebe173a6f5185
David <dafm1996@gmail.com>
2020-01-07 15:54:22 +0000

    sending start to peripherals of game, and receiving and storing activity time

--- server/routes/peripheral.js
+++ server/routes/peripheral.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const router = new express.Router()
+const connection = require('../db/mysql.js');
 var mqtt = require('mqtt')
-var client = mqtt.connect('mqtt://192.168.1.9')
+var client = mqtt.connect('mqtt://192.168.0.122')
 let connectedDevices = []
 
 client.on('connect', function () {
     client.subscribe('connected');
 })
 
-
 client.on('message', function (topic, message) {
     if (topic === 'connected') {
         connectedDevices = (JSON.parse(message.toString()))
+    }
+    //TODO receive different topics and store accordingly
+    else if (topic.match(/^metrics\/\d+\/activityTime/)) {
+        const obj = JSON.parse(message.toString());
+        let q = "UPDATE `BLE_Sports_Tracker`.`Metrics` "
+            + "SET `still_time` = ?, `walking_time` = ?, `running_time` = ? "
+            + "WHERE (`ppg_id` = ?)";
+        connection.query(q, [obj.activityTime.STILL, obj.activityTime.WALKING, obj.activityTime.RUNNING, obj.ppgId], function (err, result) {
+            if (err) {
+                return res.status(400).send('DB error')
+            }
+        })
     }
 })
 
@@ -62,6 +74,54 @@ router.get('/tracking', (req, res) => {
             return res.send(JSON.parse(data));
         }
     });
+})
+
+router.post('/peripherals/:gameId/start', (req, res) => {
+    let q = 'SELECT * FROM BLE_Sports_Tracker.Player_Peripheral_Game ppg '
+        + 'inner join Peripheral ph on ppg.peripheral_id = ph.idPeripheral '
+        + 'where ppg.game_id = ?';
+
+    connection.query(q, [req.params.gameId], function (err, result) {
+        if (err) {
+            return res.status(400).send('DB error')
+        }
+        for (const item of result) {
+            console.log(item.peripheralAddress)
+            client.publish('operation',
+                JSON.stringify({
+                    operation: 'startRaw',
+                    address: item.peripheralAddress,
+                    gameId: item.game_id,
+                    ppgId: item.idPlayer_Peripheral_Game,
+                    peripheralPosition: item.peripheral_position
+                }))
+        }
+        //TODO send body part and subscribed to correspondent topic(s)
+        client.subscribe('metrics/' + req.params.gameId + "/activityTime");
+        res.send();
+    })
+})
+
+router.post('/peripherals/:gameId/stop', (req, res) => {
+    let q = 'SELECT * FROM BLE_Sports_Tracker.Player_Peripheral_Game ppg '
+        + 'inner join Peripheral ph on ppg.peripheral_id = ph.idPeripheral '
+        + 'where ppg.game_id = ?';
+
+    connection.query(q, [req.params.gameId], async function (err, result) {
+        if (err) {
+            return res.status(400).send('DB error')
+        }
+        for (const item of result) {
+            console.log(item.peripheralAddress)
+            client.publish('operation',
+                JSON.stringify({
+                    operation: 'stopRaw',
+                    address: item.peripheralAddress,
+                }))
+        }
+        client.unsubscribe('metrics/' + req.params.gameId + "/activityTime");
+        res.send();
+    })
 })
 
 module.exports = router
